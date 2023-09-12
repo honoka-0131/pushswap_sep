cd5787c5c2fdf3b91188b03e651b946e3c1dce86
hmakida <hmakida@student.42tokyo.jp>
2023-09-12 15:33:51 +0900

    sort OK

--- over_seven.c
+++ over_seven.c
@@ -6,7 +6,7 @@
 /*   By: hmakida <hmakida@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 23:02:23 by hmakida           #+#    #+#             */
-/*   Updated: 2023/09/12 15:17:29 by hmakida          ###   ########.fr       */
+/*   Updated: 2023/09/12 15:32:38 by hmakida          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,15 +52,15 @@ void    sort_over_seven(t_stacks *stacks)
         num += 10;
         group ++;
     }
-    while (stacks->stack_b->count != 0)
+    while (stacks->stack_b->count > 1)
     {
         tmp = stacks->stack_b->top;
- //   printf("tmp->group %d\n", tmp->group);
+     //   printf("count: a %d, b %d\n", stacks->stack_a->count, stacks->stack_b->count);
         group = tmp->group;
-        while (tmp->group == group)
+        while (stacks->stack_b->count > 0 && tmp->group == group)
         {
             max = find_max(stacks->stack_b);
-            printf("max = %d\n", max);
+     //       printf("max = %d\n", max);
             if (tmp->comp == max)
             {
                 while (stacks->stack_b->top->comp != max)
@@ -71,23 +71,24 @@ void    sort_over_seven(t_stacks *stacks)
             else
                 tmp = tmp->next;
         }
-        printf("now tmp = %d, group %d\n", tmp->comp, tmp->group);
+   //     printf("now tmp = %d, group %d\n", tmp->comp, tmp->group);
         tmp = stacks->stack_b->bottom;
-        while (tmp->group == group)
+        while (stacks->stack_b->count > 0 && tmp->group == group)
         {
             max = find_max(stacks->stack_b);
-            printf("max = %d\n", max);
+            //printf("max = %d\n", max);
             if (tmp->comp == max)
             {
-                printf("入ってるかチェック\n");
-                while (stacks->stack_b->bottom->comp != max)
+              //  printf("入ってるかチェック\n");
+                while (stacks->stack_b->top->comp != max)
                     rev_rotate_b(stacks);
+              //  printf("stackB top %d\n", stacks->stack_b->top->comp);
                 push_a(stacks);
                 tmp = stacks->stack_b->bottom;
             }
             else
                 tmp = tmp->prev;
-            printf("check tmp = %d, group %d\n", tmp->comp, tmp->group);
+ //           printf("check tmp = %d, group %d\n", tmp->comp, tmp->group);
         }
     }
     return ;
